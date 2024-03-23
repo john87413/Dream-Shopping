ed23b645dab1e2deb5c9af166d4d959cfb718e46
ADV\jason-jd.chen <jason-jd.chen@advantest.com>
2024-03-23 08:05:20 +0800

    Create NavbarComponent

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,13 +1,20 @@
 <template>
-  <div class="navbar position-fixed w-100 d-flex
-  justify-content-between align-items-center py-4 px-5">
-    <div class="navbar-bg" :class="{'active': scrollDown}"></div>
-    <router-link class="navbar-logo text-light fs-4" :to="`/`" @click="changePage">
+  <div
+    class="navbar position-fixed w-100 d-flex justify-content-between align-items-center py-4 px-5"
+  >
+    <div class="navbar-bg" :class="{ active: scrollDown }"></div>
+    <router-link
+      class="navbar-logo text-light fs-4"
+      :to="`/`"
+      @click="changePage"
+    >
       RingRing
     </router-link>
-    <div class="navbar-container d-flex justify-content-center align-items-center">
-      <div class="navbar-control d-flex" v-if="scrollDown || showIcon">
-        <router-link class="me-4 scale-hover" :to="`/about`">
+    <div
+      class="navbar-container d-flex justify-content-center align-items-center"
+    >
+      <div v-if="scrollDown" class="navbar-control d-flex">
+        <router-link class="me-4 scale-hover" :to="`/shop`">
           <span class="material-icons fs-3"> storefront </span>
         </router-link>
         <div class="pop-up">
@@ -17,18 +24,20 @@
             class="d-block position-relative me-4 scale-hover"
           >
             <span class="nav-num"> 5 </span>
-            <span class="material-icons fs-3">
-              favorite
-            </span>
+            <span class="material-icons fs-3"> favorite </span>
           </a>
-          <div class="nav-dropdown bg-light" ref="navFavoriteDropdown" @click.stop>
+          <div
+            class="nav-dropdown bg-light"
+            ref="navFavoriteDropdown"
+            @click.stop
+          >
             <div v-if="favoriteProduct.length <= 0">
-              <p class="text-dark text-center p-5">收藏清單目前沒有商品喔</p>
-              <div class="p-5">
+              <p class="text-dark text-center p-3">收藏清單目前沒有商品喔</p>
+              <div class="p-3">
                 <router-link
-                  :to="`/about`"
+                  :to="`/shop`"
                   class="btn btn-secondary secondary-hover text-white w-100"
-                  >
+                >
                   前往商店
                 </router-link>
               </div>
@@ -38,27 +47,76 @@
         <div class="pop-up">
           <a
             @click.stop.prevent="openNavList('cart')"
-            v-if="showCart"
             href="#"
             class="d-block position-relative scale-hover me-4"
           >
-            <span class="nav-num">
-              5
-            </span>
+            <span v-if="cart.length" class="nav-num"> {{ cart.length }} </span>
             <span class="material-icons fs-3"> shopping_cart </span>
           </a>
           <div class="nav-dropdown bg-light" ref="navCartDropdown" @click.stop>
-            <div>
-              <p class="text-dark text-center p-5">購物車目前沒有商品喔</p>
-              <div class="p-5">
+            <div
+              v-if="popupLoading"
+              class="d-flex justify-content-center align-items-center
+              position-absolute w-100 h-100 top-0 bg-light bg-opacity-50 z-1"
+            >
+              <div class="spinner-border text-dark" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            </div>
+            <div v-if="cart.length <= 0">
+              <p class="text-dark text-center p-3">購物車目前沒有商品喔</p>
+              <div class="p-3">
                 <router-link
-                  :to="`/about`"
+                  :to="`/cart`"
                   class="btn btn-secondary secondary-hover text-white w-100"
                 >
-                  前往商店
+                  前往購物車
                 </router-link>
               </div>
             </div>
+            <template v-else>
+              <ul class="nav-dropdown-body">
+                <li
+                  v-for="item in cart"
+                  class="d-flex text-dark align-items-center
+                  justify-content-between border-bottom p-3"
+                  :key="item.id"
+                >
+                  <div class="d-flex align-items-center">
+                    <img
+                      class="w-30 me-3"
+                      :src="item.product.imageUrl"
+                      :alt="item.product.title"
+                    />
+                    <div class="">
+                      <p>{{ item.product.title }}</p>
+                      <p class="fs-7">
+                        {{ item.qty }} * NT${{ item.product.price }} = NT${{
+                          item.final_total
+                        }}
+                      </p>
+                    </div>
+                  </div>
+                  <a
+                    href="#"
+                    class="text-dark"
+                    @click.prevent="removeCartItem(item.id)"
+                  >
+                    <span class="material-icons scale-hover">
+                      delete_forever
+                    </span>
+                  </a>
+                </li>
+              </ul>
+              <div class="px-5 py-3">
+                <router-link
+                  :to="`/cart`"
+                  class="btn btn-secondary secondary-hover text-white w-100"
+                >
+                  前往購物車
+                </router-link>
+              </div>
+            </template>
           </div>
         </div>
       </div>
@@ -85,7 +143,7 @@
             </li>
             <li class="nav-item p-4">
               <router-link
-                :to="`/products`"
+                :to="`/shop`"
                 class="nav-link"
                 @click="changePage"
                 @mouseenter="changeImg('shop')"
@@ -184,6 +242,9 @@
 </template>
 
 <script>
+import { mapActions, mapState } from 'pinia';
+import cartStore from '@/store/cartStore';
+
 export default {
   props: {
     page: {
@@ -196,16 +257,16 @@ export default {
       scrollDown: false,
       nowPage: '',
       hoverPage: '',
-      carts: [],
       favoriteProduct: [],
-      showIcon: false,
-      showCart: true,
-      type: '',
-      dropdownData: [],
-      dropDownLoading: false,
+      popupType: '',
+      popupLoading: false,
     };
   },
+  computed: {
+    ...mapState(cartStore, ['cart']),
+  },
   methods: {
+    ...mapActions(cartStore, ['readCart', 'delCartItem']),
     openNav() {
       this.$refs.toggle.classList.toggle('open');
       this.$refs.navbar.classList.toggle('open');
@@ -228,23 +289,18 @@ export default {
         this.scrollDown = false;
       }
     },
-    openNavList(type) {
-      this.type = type;
-      if (type === 'cart') {
+    openNavList(popupType) {
+      this.popupType = popupType;
+      if (popupType === 'cart') {
         this.$refs.navCartDropdown.classList.toggle('show');
         this.$refs.navFavoriteDropdown.classList.remove('show');
-      } else if (type === 'favorite') {
+      } else if (popupType === 'favorite') {
         this.$refs.navFavoriteDropdown.classList.toggle('show');
         this.$refs.navCartDropdown.classList.remove('show');
       } else {
         this.$refs.navCartDropdown.classList.remove('show');
         this.$refs.navFavoriteDropdown.classList.remove('show');
       }
-    },
-    goToTarget(page) {
-      this.$refs.navCartDropdown.classList.remove('show');
-      this.$refs.navFavoriteDropdown.classList.remove('show');
-      this.$router.push(`/${page}`);
     },
     closeDropDown() {
       if (this.$refs.navCartDropdown) {
@@ -254,10 +310,23 @@ export default {
         this.$refs.navFavoriteDropdown.classList.remove('show');
       }
     },
+    // get all cart item
+    async getCart() {
+      this.popupLoading = true;
+      await this.readCart();
+      this.popupLoading = false;
+    },
+    // remove single cart item
+    async removeCartItem(id) {
+      this.popupLoading = true;
+      await this.delCartItem(id);
+      this.popupLoading = false;
+    },
   },
   mounted() {
     window.addEventListener('scroll', this.handleScroll);
     document.body.addEventListener('click', this.closeDropDown);
+    this.getCart();
   },
   unmounted() {
     window.removeEventListener('scroll', this.handleScroll);
@@ -265,299 +334,3 @@ export default {
   },
 };
 </script>
-
-<style lang="scss">
-.navbar {
-  z-index: 1020;
-  transition: background-image 0.5s;
-
-  .navbar-bg {
-    position: absolute;
-    top: 0px;
-    right: 0px;
-    bottom: 0px;
-    left: 0px;
-    background: url("/images/nav-bg.jpg") no-repeat center;
-    background-size: cover;
-    box-shadow: 0px 5px 10px rgba(#000, 0.1);
-    z-index: -1;
-    opacity: 0;
-    transition: opacity 0.3s;
-
-    &.active{
-      opacity: 1;
-    }
-  }
-
-  .navbar-logo {
-    z-index: 1060;
-    font-family: "Dancing Script", cursive;
-  }
-
-  .navbar-container {
-    .pop-up{
-      .nav-num {
-        position: absolute;
-        top: -10px;
-        left: 16px;
-        width: 18px;
-        height: 18px;
-        font-size: 14px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-        background-color: #bf7f5a;
-        color: #fff;
-        box-shadow: -1px 1px 5px rgba(0, 0, 0, 0.5);
-      }
-
-      .nav-dropdown {
-        width: 320px;
-        position: absolute;
-        top: 72px;
-        right: 0px;
-        opacity: 0;
-        border-radius: 4px;
-        pointer-events: none;
-        overflow-y: hidden;
-        transition: all 0.5s;
-      }
-
-      .nav-dropdown.show {
-        opacity: 1;
-        right: 24px;
-        pointer-events: all;
-      }
-    }
-
-    .navbar-toggle {
-      position: relative;
-      z-index: 1060;
-      width: 72px;
-      height: 18px;
-      cursor: pointer;
-
-      span {
-        position: absolute;
-        top: 0;
-        left: 0;
-        display: block;
-        width: 100%;
-        height: 2px;
-        background-color: rgba(#fff, 0.8);
-        transition: all 0.8s;
-
-        &:nth-child(2) {
-          top: 8px;
-        }
-
-        &:nth-child(3) {
-          width: 48px;
-          height: 2px;
-          top: 16px;
-          left: auto;
-          right: 0;
-        }
-      }
-
-      &.open {
-        width: 36px;
-
-        span {
-          &:nth-child(1) {
-            top: 8px;
-            transform: rotate(135deg);
-          }
-
-          &:nth-child(2) {
-            opacity: 0;
-            left: 60px;
-          }
-          &:nth-child(3) {
-            width: 100%;
-            top: 8px;
-            transform: rotate(-135deg);
-          }
-        }
-      }
-    }
-  }
-
-  .navbar-collapse {
-    position: fixed;
-    top: -100vh;
-    left: 100%;
-    display: flex;
-    align-items: center;
-    position: absolute;
-    width: 100%;
-    height: 100vh;
-    background: url("/images/nav-bg.jpg") no-repeat center;
-    background-size: cover;
-    padding: 0 12px;
-    transition: all 0.6s;
-
-    &.open {
-      top: 0;
-      left: 0;
-      z-index: 1050;
-      .nav-item {
-        position: relative;
-        opacity: 0;
-        top: 100px;
-        animation-name: nav-link-fade;
-        animation-duration: 0.5s;
-        animation-timing-function: linear;
-        animation-fill-mode: forwards;
-        &:nth-child(1) {
-          animation-delay: 0.5s;
-        }
-        &:nth-child(2) {
-          animation-delay: 0.7s;
-        }
-        &:nth-child(3) {
-          animation-delay: 0.9s;
-        }
-        &:nth-child(4) {
-          animation-delay: 1.1s;
-        }
-      }
-      .nav-circle {
-        animation: nav-link-fade 0.5s linear 0.2s forwards;
-      }
-    }
-
-    .nav-menu{
-      width: 50%;
-
-      .nav-item {
-        &:hover {
-          .nav-title {
-            &::before {
-              width: 100%;
-            }
-          }
-        }
-
-        .nav-link {
-          display: flex;
-          align-items: center;
-          &.router-link-exact-active {
-            .nav-title {
-              &::before {
-                width: 100%;
-              }
-            }
-          }
-        }
-
-        .nav-number {
-          font-size: 12px;
-          margin-right: 16px;
-          color: #fff;
-        }
-
-        .nav-title {
-          font-size: 28px;
-          letter-spacing: 3px;
-          margin-right: 60px;
-          color: #fff;
-          font-family: 'Abril Fatface', cursive;
-          position: relative;
-          &::after {
-            content: '';
-            position: absolute;
-            display: inline-block;
-            height: 30px;
-            right: -15px;
-            top: 50%;
-            width: 1px;
-            border-left: 1px solid #fff;
-            transform: translateY(-50%) skewX(-45deg);
-            transform-origin: left bottom;
-          }
-          &::before {
-            content: '';
-            position: absolute;
-            display: block;
-            width: 0;
-            height: 10px;
-            bottom: 8px;
-            z-index: -1;
-            transition: all 0.5s;
-            background-color: rgba(#bf7f5a, 0.8);
-          }
-        }
-
-        .nav-title-ch {
-          font-size: 16px;
-          color: rgba(#fff, 0.6);
-        }
-      }
-    }
-
-    .nav-circle {
-      position: relative;
-      opacity: 0;
-      padding-top: 40%;
-      width: 40%;
-      &::before {
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 105%;
-        border-radius: 50em;
-        top: -2.5%;
-        left: 0;
-        border: 1px dashed #fff;
-        z-index: -1;
-        opacity: 0.6;
-        animation: rotate linear 10s infinite;
-      }
-      &::after {
-        content: '';
-        position: absolute;
-        width: 100%;
-        height: 110%;
-        border-radius: 50em;
-        top: -5%;
-        left: 0%;
-        border: 1px dashed #fff;
-        z-index: -1;
-        opacity: 0.4;
-        animation: rotate linear 10s infinite reverse;
-      }
-      img {
-        position: absolute;
-        top: 0;
-        left: 0;
-        opacity: 0;
-        transition: all 0.5s;
-        transform: rotateY(-180deg);
-        &.show {
-          z-index: -1;
-          opacity: 1;
-          transform: rotateY(0deg);
-        }
-        &.hoverImg {
-          opacity: 1;
-          transform: rotateY(0deg);
-          transform-style: preserve-3d;
-        }
-      }
-    }
-  }
-}
-
-@keyframes nav-link-fade {
-  0% {
-    opacity: 0;
-    top: 100px;
-  }
-  100% {
-    opacity: 1;
-    top: 0;
-  }
-}
-</style>
