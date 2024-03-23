171b16b400d90cc12be7d096eefdd7252a47c285
ADV\jason-jd.chen <jason-jd.chen@advantest.com>
2024-03-23 08:07:16 +0800

    create contact & about view, create swiper component

---
+++ src/components/SwipperComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <swiper-container
+    :loop="false"
+    :space-between="10"
+    :navigation="false"
+    thumbs-swiper=".my-thumbs"
+    class="my-swiper"
+  >
+    <swiper-slide
+      ><img :src="product.imageUrl" :alt="product.title"
+    /></swiper-slide>
+    <swiper-slide v-for="(img, index) in product.imagesUrl" :key="index">
+      <img :src="product.imagesUrl[index]" :alt="product.title" />
+    </swiper-slide>
+  </swiper-container>
+  <swiper-container
+    :loop="false"
+    :space-between="10"
+    :slides-per-view="6"
+    :watch-slides-visibility="true"
+    :watch-slides-progress="true"
+    class="my-thumbs mt-4"
+  >
+    <swiper-slide
+      ><img :src="product.imageUrl" :alt="product.title"
+    /></swiper-slide>
+    <swiper-slide v-for="(img, index) in product.imagesUrl" :key="index">
+      <img :src="product.imagesUrl[index]" :alt="product.title" />
+    </swiper-slide>
+  </swiper-container>
+</template>
+<script>
+import { register } from 'swiper/element/bundle';
+
+export default {
+  props: {
+    product: Object,
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+  },
+  mounted() {
+    register();
+  },
+};
+</script>
--- src/views/customer/ContactView.vue
+++ src/views/customer/ContactView.vue
@@ -1,45 +1,49 @@
 <template lang="html">
   <div class="contact-view container">
-    <ol class="breadcrumb mb-5">
+    <ol class="breadcrumb mb-3">
       <li class="breadcrumb-item"><router-link :to="`/`">首頁</router-link></li>
       <li class="breadcrumb-item active">聯絡我們</li>
     </ol>
-    <div class="py-5 border-bottom border-top border-light mb-5">
+    <div class="py-5 border-bottom border-top border-light mb-9">
       <h3 class="fs-2 text-center">美麗，從這裡出發</h3>
     </div>
-    <div class="contact-container">
+    <div class="contact-container px-10">
       <div class="d-flex mb-5">
         <h3 class="title">聯絡我們</h3>
         <p class="opacity-50 fs-5">Contact Us</p>
       </div>
-      <div class="row align-items-center flex-column flex-md-row pb-5 mb-5">
-        <div class="col-12 col-md-6 col-lg-4 offset-lg-1 mb-10">
-          <img
-            class="rounded-3 w-80 w-md-100 mx-auto"
-            src="https://storage.googleapis.com/vue-course-api.appspot.com/chiayu/1625638666278.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=jTNMXMwJRjARojrEpu0R1uWFZ%2B9CXpB%2BeDqBcXlJ6IeeHdSFDKviG2dBEuAx3qXbgrv4D4sa23fyVoI6JpHtDMMY2Kf0xHAXjFGDR4WfsPfwow7Dka%2BaHGW7Gphr0mdoJ8uHxSOPvJF%2B6F2FxU8WSwDnEf6GIeDQvpxKv1D9Kkd3mitBguwDh%2FsEvQ2ujLnFlWgHts85wZJIG2cfE28vTOMoE%2B6O9tyDWt32FiOqcA7fVUHJ5hosungoUN0iCo99JMAd3rfAZUqjLxknfUO4rjLFtorkkHgIjBrjQmAH1zR87Cfqk9Ivsa5pdC0qtGZDdEzSOIimFAbVqvs5BxF2gQ%3D%3D"
-            alt="RingRing"
-          />
-        </div>
-        <div class="col-12 col-md-6 col-lg-4 offset-lg-2 mb-10">
-          <ul class="mb-7">
-            <li class="lh-lg letter-spacing-1">客服電話：(02)-213-1234</li>
-            <li class="lh-lg letter-spacing-1">客服信箱：ringring@gmail.com</li>
-            <li class="lh-lg letter-spacing-1">
-              客服時間：週一至週五：9:00 am - 18:00 pm
-            </li>
-            <li class="lh-lg letter-spacing-1">Instgram：@ringring</li>
-            <li class="lh-lg letter-spacing-1">Facebook：Ringring</li>
-          </ul>
-          <div class="d-flex social-icon">
-            <a class="scale-hover me-5" href="#" target="_blank">
-              <span><img src="/images/fb.svg" alt="fb" /> </span>
-            </a>
-            <a class="scale-hover me-5" href="#" target="_blank">
-              <span><img src="/images/ig.svg" alt="ig" /> </span>
-            </a>
-            <a class="scale-hover" href="#" target="_blank">
-              <span><img src="/images/line.svg" alt="line" /> </span>
-            </a>
+      <div class="container">
+        <div class="row align-items-center justify-content-between mb-9">
+          <div class="col-5 p-0 pe-3">
+            <img
+              class="rounded-3"
+              src="https://storage.googleapis.com/vue-course-api.appspot.com/chiayu/1625638666278.jpg?GoogleAccessId=firebase-adminsdk-zzty7%40vue-course-api.iam.gserviceaccount.com&Expires=1742169600&Signature=jTNMXMwJRjARojrEpu0R1uWFZ%2B9CXpB%2BeDqBcXlJ6IeeHdSFDKviG2dBEuAx3qXbgrv4D4sa23fyVoI6JpHtDMMY2Kf0xHAXjFGDR4WfsPfwow7Dka%2BaHGW7Gphr0mdoJ8uHxSOPvJF%2B6F2FxU8WSwDnEf6GIeDQvpxKv1D9Kkd3mitBguwDh%2FsEvQ2ujLnFlWgHts85wZJIG2cfE28vTOMoE%2B6O9tyDWt32FiOqcA7fVUHJ5hosungoUN0iCo99JMAd3rfAZUqjLxknfUO4rjLFtorkkHgIjBrjQmAH1zR87Cfqk9Ivsa5pdC0qtGZDdEzSOIimFAbVqvs5BxF2gQ%3D%3D"
+              alt="RingRing"
+            />
+          </div>
+          <div class="col-5 p-0 ps-3">
+            <ul class="mb-4">
+              <li class="lh-lg letter-spacing-1">客服電話：(02)-213-1234</li>
+              <li class="lh-lg letter-spacing-1">
+                客服信箱：ringring@gmail.com
+              </li>
+              <li class="lh-lg letter-spacing-1">
+                客服時間：週一至週五：9:00 am - 18:00 pm
+              </li>
+              <li class="lh-lg letter-spacing-1">Instgram：@ringring</li>
+              <li class="lh-lg letter-spacing-1">Facebook：Ringring</li>
+            </ul>
+            <div class="d-flex social-icon">
+              <a class="scale-hover me-3" href="#" target="_blank">
+                <span><img src="/images/fb.svg" alt="fb" /> </span>
+              </a>
+              <a class="scale-hover me-3" href="#" target="_blank">
+                <span><img src="/images/ig.svg" alt="ig" /> </span>
+              </a>
+              <a class="scale-hover" href="#" target="_blank">
+                <span><img src="/images/line.svg" alt="line" /> </span>
+              </a>
+            </div>
           </div>
         </div>
       </div>
@@ -60,9 +64,7 @@
         <p>為保障買家權益， 所有商品皆享有30日的售後服務。</p>
       </li>
       <li class="col border-end border-light">
-        <span class="material-icons fs-1 mb-3">
-          question_answer
-        </span>
+        <span class="material-icons fs-1 mb-3"> question_answer </span>
         <h3 class="fs-4 mb-4">客服服務</h3>
         <p>如購買過程及收到商品有任何問題，歡迎聯繫我們， 我們會盡快回覆您。</p>
       </li>
